6fb6769730e8c7de33e746878eba68abe012716a
toantq.uit <quoctoan0629@gmail.com>
2019-11-18 01:10:44 +0700

    Merge code

---
+++ Game_Aladdin/Bat.cpp
@@ -0,0 +1,78 @@
+#include "Bat.h"
+#include "define.h"
+#include "Aladdin.h"
+
+void Bat::GetBoundingBox(float &left, float &top, float &right, float &bottom)
+{
+	left = x;
+	top = y;
+	right = x + BAT_BBOX_WIDTH;
+
+	if (state == GUARDIAN_STATE_DIE)
+		bottom = y + GUARDIAN_BBOX_HEIGHT_DIE;
+	else
+		bottom = y + BAT_BBOX_HEIGHT;
+}
+
+void Bat::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+{
+	CGameObject::Update(dt, coObjects);
+	x += dx;
+	y += dy;
+	if (state == BAT_STATE_DIE)
+	{
+		SetSpeed(0.0f, 0.0f);
+		return;
+	}
+
+	if (vx < 0 && y > 950) {
+		y = 950;
+		vx = -vx;
+	}
+	if (vx < 0 && y == 950) {
+		vy = -vy;
+	}
+	float check = this->x - Aladdin::XforGet;
+	if (check < 150)
+		this->SetState(BAT_STATE_ATTACK);
+
+
+
+}
+
+void Bat::Render()
+{
+	int ani;
+
+	if (state == BAT_STATE_DIE) {
+		vx = 0;
+		vy = 0;
+		return;
+	}
+	if (state == BAT_STATE_WAIT)
+		ani = BOSSBAT_ANI_WAIT;
+	else
+		ani = BOSSBAT_ANI_ATTACK;
+
+
+	animations[ani]->Render(x, y);
+	RenderBoundingBox();
+}
+
+void Bat::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case BAT_STATE_WAIT:
+		vx = 0;
+		vy = 0;
+		break;
+	case BAT_STATE_ATTACK:
+		vx = -0.02f;
+		vy = 0.02f;
+		break;
+	}
+
+}
+
